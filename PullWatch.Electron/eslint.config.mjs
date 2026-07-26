5368d933ea2c9b87348885d4624c2af8565af675
afotescu <artiomfotescu1@gmail.com>
2026-07-26 22:45:00 +0300

    electron forge init + update ts/vite/eslint

---
+++ PullWatch.Electron/eslint.config.mjs
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript';
+import { importX } from 'eslint-plugin-import-x';
+import globals from 'globals';
+import { configs as tseslintConfigs } from 'typescript-eslint';
+
+export default defineConfig(
+  {
+    ignores: ['.vite/**', 'out/**'],
+  },
+  {
+    files: ['**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslintConfigs.recommended,
+      importX.flatConfigs.recommended,
+      importX.flatConfigs.electron,
+      importX.flatConfigs.typescript,
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    settings: {
+      'import-x/resolver-next': [createTypeScriptImportResolver()],
+    },
+  },
+);
